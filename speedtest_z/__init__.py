abcb8293346c1aa09e160fbe4ca0a1ad0f322e5c
AIKAWA Shigechika <shige@aikawa.jp>
2026-08-08 21:17:59 +0900

    chore(main): release 0.12.0 (#60)

--- speedtest_z/__init__.py
+++ speedtest_z/__init__.py
@@ -28,4 +28,4 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("speedtest-z")
 except PackageNotFoundError:
-    __version__ = "0.11.0"
+    __version__ = "0.12.0"
